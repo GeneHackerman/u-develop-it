a9b2e314b1f01cbbbd45c4701e73cf3c92f84661
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-01 11:16:54 -0500

    added query of database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +8,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Kgroove70%dmg',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // this GET is test route to make sure 3001 is connecting
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: 'Hello World'
 //     });
 // });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 // must be placed below all else as it is a catchall route
